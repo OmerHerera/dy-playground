fc0c4d48f777f6f084ee449227a1ed1f7d965a69
OmerHerera <omer.herera@gmail.com>
2023-10-21 21:34:14 +0300

    feature(first commit): Add code for first commit

---
+++ src/index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+window.addEventListener("beforeunload", function (e) {
+  // localStorage.removeItem('_qa_sectionId');
+  // localStorage.removeItem('_qa_cdn');
+  for (var key in localStorage) {
+    if (key.indexOf('_qa_') === 0) {
+      localStorage.removeItem(key);
+    }
+  }
+});
+
+function getValue(key) {
+  // First try to get it from URL querystring
+  console.log(`Trying to get: ${key} from queryString`);
+  const urlParams = new URLSearchParams(window.location.search);
+  let value = urlParams.get(key);
+  if (value) {
+    return value;
+  } else {
+    console.log(`Not in queryString, Trying to get: _qa_${key} from localStorage`);
+    value = localStorage.getItem(`_qa_${key}`);
+    return value;
+  }
+}
+
+function saveURLSearchParams() {
+  if (window.location.search) {
+   const urlParams = new URLSearchParams(window.location.search);
+    for (const [key, value] of urlParams.entries()) {
+      localStorage.setItem(`_qa_${key}`, value);
+    }
+  }
+}
+
+function setURLSearchParams() {
+  // if not queryParams try to get if from localStorage
+  if (!window.location.search) {
+    for (var key in localStorage) {
+      if (key.indexOf('_qa_') === 0) {
+        const value = localStorage.getItem(key);
+        const url = new URL(window.location);
+        const normalizeKey = key.replace(/_qa_/g, '')
+        url.searchParams.set(`${normalizeKey}`, `${value}`);
+        window.history.pushState({}, '', url);
+      }
+    }
+  }
+}
+
+function insertScript(path) {
+  const script = document.createElement("script");
+  script.src = path;
+  script.type = "text/javascript";
+  script.async = true;
+  document.head.appendChild(script);
+}
+function init() {
+  console.log('App inserting DY scripts');
+  const sectionId = getValue('sectionId');
+  const cdn = getValue('cdn') || 'cdn.dynamicyield.com';
+  const fullPath = `https://${cdn}/api/${sectionId}`;
+  if (sectionId && fullPath) {
+    insertScript(`${fullPath}/api_dynamic.js`);
+    insertScript(`${fullPath}/api_static.js`);
+    saveURLSearchParams();
+    setURLSearchParams()
+  } else {
+    console.log(`Missing one of the following sectionId: ${sectionId} fullPath: ${fullPath}`);
+  }
+}
+init();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
